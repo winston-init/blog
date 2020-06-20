94cd1e587432a0966a22a0afa541904da36771bc
winston-init <winston.init@gmail.com>
2020-06-19 22:41:33 -0300

    middlewires morgan and helmet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const home = require('./routes/home')
 const posts = require('./routes/posts')
 
+const morgan = require('morgan')
+const helmet = require('helmet')
+
 const app = express()
 
 app.use(express.json())
 app.use('/api', home)
 app.use('/api/posts', posts)
+app.use(morgan('dev'))
+app.use(helmet())
 
 const port = process.env.PORT || 3000
 
