716905bde8d22d30f325ffe12921239454b10659
winston-init <winston.init@gmail.com>
2020-06-21 22:48:33 -0300

    crud operations methods added

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -13,9 +13,54 @@ db.once('open', () => {
 })
 
 const postSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  tags: [String],
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  tags: { type: [String], required: true },
   date: { type: Date, default: Date.now },
-  isPublished: Boolean,
+  isPublished: { type: Boolean, required: true },
 })
+
+const Post = mongoose.model('Post', postSchema)
+
+async function createPost() {
+  const post = new Post({
+    title: 'Update 1.0',
+    author: 'Winston',
+    tags: ['update', 'version'],
+    isPublished: true,
+  })
+
+  const result = await post.save()
+  console.log(result)
+}
+
+async function getPosts() {
+  const pageNumber = 1
+  const pageSize = 10
+
+  const posts = await Post.find({ author: 'Winston', isPublished: true })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({ title: 1 })
+    .countDocuments()
+
+  console.log(posts)
+}
+
+async function updatePost(id) {
+  const post = await Post.findByIdAndUpdate(
+    id,
+    {
+      $set: { title: 'Jack', isPublished: false },
+    },
+    { new: true }
+  )
+  console.log(post)
+}
+
+async function removePost(id) {
+  const result = await Post.findByIdAndRemove(id)
+  console.log(result)
+}
+
+removePost('5eee8c84d71bb20c277794f2')
