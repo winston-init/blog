d72872163d384e306db60860ffcf0a18c4b92bb1
winston-init <winston.init@gmail.com>
2020-06-23 11:34:34 -0300

    connection to database and database's handler added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const posts = require('./routes/posts')
 
 const morgan = require('morgan')
 const helmet = require('helmet')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -12,6 +13,19 @@ app.use('/api', home)
 app.use('/api/posts', posts)
 app.use(morgan('dev'))
 app.use(helmet())
+
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Database connected...')
+})
 
 const port = process.env.PORT || 3000
 
