715eee1d158dc7d3f917df4e14dfd8684ea8c5c1
winston-init <winston.init@gmail.com>
2020-06-25 19:42:22 -0300

    joi schema created

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
+const Joi = require('@hapi/joi')
+
+const validatePost = post => {
+  const schema = Joi.object({
+    title: Joi.string().min(2).max(50).required(),
+    author: Joi.string().min(2).max(50).required(),
+    isPublished: Joi.boolean().required(),
+  })
+
+  return schema.validate(post)
+}
 
 const PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  tags: { type: [String], required: true },
+  tags: { type: [String] },
   date: { type: Date, default: Date.now },
   isPublished: { type: Boolean, required: true },
 })
 
-module.exports = mongoose.model('Post', PostSchema)
+exports.Post = mongoose.model('Post', PostSchema)
+exports.validate = validatePost
