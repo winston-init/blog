0b16eed2e863dc6f9ceb67043c00e1095c4c9bdc
winston-init <winston.init@gmail.com>
2020-06-27 21:55:25 -0300

    destructuring from prop

--- frontend/src/components/PostItem.js
+++ frontend/src/components/PostItem.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class PostItem extends Component {
   render() {
+    const { title } = this.props.post
+
     return (
-      <div>
-        <p>{this.props.post.title}</p>
+      <div style={{ background: '#ccc' }}>
+        <p>{title}</p>
       </div>
     )
   }
+}
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
 }
 
 export default PostItem
