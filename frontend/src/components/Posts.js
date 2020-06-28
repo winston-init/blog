86daeb212f27babdfb334ac7fb822eca71f56b0f
winston-init <winston.init@gmail.com>
2020-06-27 21:55:50 -0300

    propTypes added

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import PostItem from './PostItem'
+import PropTypes from 'prop-types'
 
 class Posts extends Component {
   render() {
-    return this.props.posts.map(post => (
-      <PostItem key={post.id} post={post} />
-    ))
+    return this.props.posts.map(post => <PostItem key={post.id} post={post} />)
   }
+}
+
+Posts.propTypes = {
+  posts: PropTypes.array.isRequired,
 }
 
 export default Posts
