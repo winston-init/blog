1d9f41a6524da756924cf4af1758412b7f95a36b
winston-init <winston.init@gmail.com>
2020-07-01 23:24:49 -0300

    base logic of post date

--- frontend/src/components/Post/index.jsx
+++ frontend/src/components/Post/index.jsx
@@ -3,8 +3,34 @@ import React, { Component } from 'react'
 import './styles.css'
 
 export class Post extends Component {
+  formatDate() {
+    const { post } = this.props
+    const date = new Date(post.date)
+    const today = new Date()
+
+    const hour = date.getHours()
+    const day = date.getDate()
+    const minute = date.getMinutes()
+    const month = date.getMonth()
+
+    if (today.getHours() >= hour && today.getMinutes() < minute) {
+      const time = Math.abs(today.getMinutes() - minute)
+      const unit = time === 1 ? 'minute' : 'minutes'
+
+      return `${time} ${unit} ago`
+    } else {
+      const time = Math.abs(today.getHours() - hour)
+      const unit = time === 1 ? 'hour' : 'minutes'
+      return `${time} ${unit} ago`
+    }
+
+    // calculate month and year
+  }
+
   render() {
     const { post } = this.props
+    const date = new Date(post.date).toLocaleDateString()
+    const time = this.formatDate()
 
     return (
       <div className='post'>
@@ -21,7 +47,7 @@ export class Post extends Component {
           </div>
           <div className='user-info'>
             <div className='user'>{post.author}</div>
-            <div className='date'>{post.date}</div>
+            <div className='date'>{time}</div>
           </div>
         </div>
       </div>
