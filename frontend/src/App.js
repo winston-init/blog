7e3ec1730d1d6af031d8724fe3d8808ce6bb2082
winston-init <winston.init@gmail.com>
2020-07-03 22:51:21 -0300

    Loader animation to promises added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './components/Header/index'
 import Main from './components/Main/index'
+import { usePromiseTracker } from 'react-promise-tracker'
+import Loader from 'react-promise-loader'
 import './App.css'
 
 const App = () => (
   <div className='App'>
     <Header />
     <Main />
+    <Loader promiseTracker={usePromiseTracker} color={'#8499b1'} />
   </div>
 )
 
