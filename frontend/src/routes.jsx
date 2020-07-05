bb6fc80403370ebd9dd936bdd60d2284f476b348
winston-init <winston.init@gmail.com>
2020-07-05 13:10:01 -0300

    return keyword added

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Main from './pages/Main/index'
 import DetailedPost from './pages/DetailedPost/index'
 
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Main} />
-      <Route path='/posts/:id' component={DetailedPost} />
-    </Switch>
-  </BrowserRouter>
-)
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Main} />
+        <Route path='/posts/:id' component={DetailedPost} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 export default Routes
