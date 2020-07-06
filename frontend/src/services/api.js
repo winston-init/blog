f0796a276070980dade2a535fd7c991852f27f8e
winston-init <winston.init@gmail.com>
2020-07-05 22:24:12 -0300

    localhost's port changed to 1300

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3000/api' })
+const api = axios.create({ baseURL: 'http://localhost:1300/api' })
 
 export default api
